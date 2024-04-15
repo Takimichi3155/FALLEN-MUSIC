7f91791fe756ae270f19aa2bf1fe974674b227ad
Takimichi3155 <166791106+Takimichi3155@users.noreply.github.com>
2024-04-15 10:09:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,22 +6,22 @@ load_dotenv()
 get_queue = {}
 
 
-API_ID = int(getenv("API_ID", "8479389"))
-API_HASH = getenv("API_HASH", "f75e6f068c8bb9b8c884484ea2c6177b")
+API_ID = int(getenv("API_ID", "26977508"))
+API_HASH = getenv("API_HASH", "396589629e6705c592bc7fe891dc6e37")
 
 ASS_HANDLER = list(getenv("ASS_HANDLER", ".").split())
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "6891505280:AAHC-b3eZuYgqw2ZgbWFC1afLYGPZ8bS9xY")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "900"))
-LOGGER_ID = int(getenv("LOGGER_ID", "-1001554007729"))
-MONGO_DB_URI = getenv("MONGO_DB_URI", "3@cluster0.4wbux.mongodb.net/?retryWrites=true&w=majority")
+LOGGER_ID = int(getenv("LOGGER_ID", "-1001806464233"))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Obito:YWntQsn1DF3nTyIA@cluster0.amvgyo6.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "1962673406").split()))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/4cd4f1b11f6ee4f52d117.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/7e69b99a9545900aef33e.jpg")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Lobe_JU")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/Oye_golgappu")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Dare_Devils_01")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/Seista_Updates")
 
-STRING_SESSION = getenv("STRING_SESSION", "lMvbNkCZIXhncQS_EtlfV2Rn4q7pGrS9-FXi1AC1L7O1p4LZ31owYnpfFJNE7hr5UNWAH5LYmFc2EeG1tiz8eP2t44U2ztMA82S00eYF2zlOeHqbuov-q2uVWdvBW5V96PQHH1hE1mXzCJc2Q-xIOrOTpGrCwQbHm8tv-oaiNvK__miFB8jjByAAAAAHTel-MA")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1962673406").split()))
+STRING_SESSION = getenv("STRING_SESSION", "BQGbpOQAJLLflnB6DZNoh6-1YMtnS-gtpULXqsVNoYvTu_p6dRnV6n9h0ZcFwfD4aAiZc7ZAxcWr1uXMM_JkbtqL24x3eC46CkKIoqbVMGCheghyd86E1lKqETzPLgDxrqGt8W-sjFunl_kmYe1xfejxb8NPu3cqKaHOa3TPgXjECVGoYg_SokpAgl75IGKO9VFN_DTvfqDvRyxZjid_yLAYY0ToWjf6unehRrKeXCDzdXS4LNxSU1vLLdjR26fjE03QiQXdhn48PVyTRh-VUI17lKMBzrwibF6wHdkqM59RO6EkVl9fxg8mXQV3p0LnCLhw4NKcGs13vr64-oGrpoG5RFQU6AAAAAGa5dquAA")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "0").split()))
